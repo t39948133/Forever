a9ef9b1b009bf74548137fdeb509b4592f4f6408
t39948133 <sotim79@hotmail.com>
2012-10-09 22:19:31 +0800

    改用Ogre  刪除

--- client/Forever/Fmath.cpp
+++
@@ -1,6 +0,0 @@
-#include "Fmath.h"
-
-float tangleL(float x1,float y1,float x2,float y2)
-{
-	return sqrt(pow(x1-x2,2)+pow(y1-y2,2));
-}
--- client/Forever/Fmath.h
+++
@@ -1,8 +0,0 @@
-#ifndef _FMATH_H_
-#define _FMATH_H_
-
-#include "math.h"
-
-float tangleL(float,float,float,float);
-
-#endif
