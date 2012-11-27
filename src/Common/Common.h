89b46e248ee5fe74123c9f90af176a4269829356
darren32 <darren.z32@msa.hinet.net>
2012-11-27 11:24:58 +0800

    專案建置

---
+++ src/Common/Common.h
@@ -0,0 +1,16 @@
+/** Copyright (c) 2012-2013 Digital Content Institute.
+  * All Rights Reserved.
+  *
+  * @檔案:     Common.h
+  * @用途:     共用
+  * @開發者: 
+  * @更新日期: 2012/11/27 */
+#ifndef _COMMON_H_
+#define _COMMON_H_
+#include <windows.h>
+#include <stdlib.h>
+#include <malloc.h>
+#include <memory.h>
+#include <tchar.h>
+
+#endif  //_COMMON_H_
---
+++ src/Server/ServerMain.cpp
@@ -0,0 +1,9 @@
+#include "..\\Common\\Common.h"
+
+int APIENTRY _tWinMain(HINSTANCE hInstance,
+                     HINSTANCE hPrevInstance,
+                     LPTSTR    lpCmdLine,
+                     int       nCmdShow)
+{
+   return 0;
+}
